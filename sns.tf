c4642160ecaedf0083eba13ebbb804f9703f3233
awiechert <awiechert@elastic2ls.com>
2023-08-09 13:27:05 +0200

    encrpyt sns topic

--- sns.tf
+++ sns.tf
@@ -1,3 +1,8 @@
 resource "aws_sns_topic" "lambda" {
   name = "ipUpdateNLB-${var.stage}-lambda"
+  kms_master_key_id = aws_kms_key.ip_update.key_id
+}
+
+resource "aws_kms_key" "ip_update" {
+  description             = "sns-encrpytion"
 }
